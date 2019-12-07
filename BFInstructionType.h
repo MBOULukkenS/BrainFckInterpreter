660c7c23027adb2fe83bf8295501a05a6bc3329f
LukkenS <326523@student.mboutrecht.nl>
2019-12-07 18:11:58 +0100

    Add None Instruction

--- BFInstructionType.h
+++ BFInstructionType.h
@@ -7,6 +7,7 @@
 
 enum BFInstructionType
 {
+    None = -1,
     dPtrIncr = '>', //increment pointer
     dPtrDecr = '<', //decrement pointer
     IncrPtrVal = '+', //increment value behind pointer
@@ -17,6 +18,6 @@ enum BFInstructionType
     loopEnd = ']' //end the loop
 };
 
-const constexpr int BFInstructionTypeList[] = {dPtrIncr, dPtrDecr, IncrPtrVal, DecrPtrVal, cWritePtrVal, cReadPtrVal, loopBegin, loopEnd };
+const constexpr BFInstructionType BFInstructionTypeList[] = { dPtrIncr, dPtrDecr, IncrPtrVal, DecrPtrVal, cWritePtrVal, cReadPtrVal, loopBegin, loopEnd };
 
 #endif //BRAINFCKINTERPRETER_BFINSTRUCTIONTYPE_H
